6f442f7aa9f923af1dd60769002765f2cdb5d3f6
jay1535 <habbujayanth@gmail.com>
2025-11-14 15:55:02 +0530

    Minor change in layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="bg-gradient-to-br from-background to-muted">
+    <div className="bg-linear-to-br from-background to-muted">
       <Header />
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
