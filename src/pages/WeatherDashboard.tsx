6bbefd5f68d6746a45d6122b160bd87333914f47
jay1535 <habbujayanth@gmail.com>
2025-11-14 17:11:37 +0530

    Added H1 tag and the refresh button

--- src/pages/WeatherDashboard.tsx
+++ src/pages/WeatherDashboard.tsx
@@ -1,11 +1,22 @@
-
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
 
 const WeatherDashboard = () => {
   return (
-    <div>
-      WeatherDashboard
+    <div className="space-y-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          // disabled={}
+          // onClick={}
+        >
+          <RefreshCcw />
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherDashboard
+export default WeatherDashboard;
