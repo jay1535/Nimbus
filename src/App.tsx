f4a69091b5934a610243cb571a79b1327280a8d5
jay1535 <habbujayanth@gmail.com>
2025-11-16 15:34:35 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "./components/ui/sonner";
 
-import { ThemeProvider } from './context/theme-provider'
-import WeatherDashboard from './pages/WeatherDashboard'
-import CityPage from './pages/CityPage'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import Layout from './components/layout'
+import { Layout } from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { WeatherDashboard } from "./pages/WeatherDashboard";
+import { CityPage } from "./pages/CityPage";
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,26 +21,22 @@ const queryClient = new QueryClient({
 });
 
 function App() {
- 
-
   return (
     <QueryClientProvider client={queryClient}>
-
-   <BrowserRouter >
-   <ThemeProvider defaultTheme='dark'>
-   <Layout>
-    <Routes>
-      <Route path='/' element={<WeatherDashboard/>}/>
-      <Route path='/city/:cityName' element={<CityPage/>}/>
-      </Routes>
-   </Layout>
-
-    </ThemeProvider>
-
-   </BrowserRouter>
-    <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+          <Toaster richColors />
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
